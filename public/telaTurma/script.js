b6be8302c6f1a4493ebdece3cd1e90f08b40354c
vinicius3.98 <vinicius3_1998@hotmail.com>
2023-10-14 13:26:17 -0300

    adicionando tela de alunos

--- public/telaTurma/script.js
+++ public/telaTurma/script.js
@@ -1,10 +1,9 @@
-//Referências para os elemetnos do modal
+//Atividades do modal
 const openModalBtn = document.getElementById("openModalBtn");
 const closeModalBtn = document.getElementById("closeModalBtn");
 const modal = document.getElementById("modal");
 const fade = document.getElementById("fade");
 
-//Função para alterar a visibilidade do modal
 const toggleModal = () => {
   modal.classList.toggle("hide");
   fade.classList.toggle("hide");
@@ -16,24 +15,25 @@ const toggleModal = () => {
 
 //Recebendo os dados inseridos no cadastrar turmas
 function enviarDados() {
-  //Referências dos elementos inseridos
-  const nomeTurmaInput = document.getElementById("nomeTurma");
-  const professorInput = document.getElementById("professor");
+  const nomeAlunoInput = document.getElementById("nomeAluno");
+  const raInput = document.getElementById("ra");
+  const turmaInput = document.getElementById("turma");
   const turnoInput = document.getElementById("turno");
 
-  //Pegando os valores inseridos nos inputs
-  const nomeTurma = nomeTurmaInput.value;
-  const professor = professorInput.value;
+  const nomeAluno = nomeAlunoInput.value;
+  const ra = raInput.value;
+  const turma = turmaInput.value;
   const turno = turnoInput.value;
 
-  //Criando uma lista de objeto com os dados das turmas
-  const dados = {
-      "Nome da Turma": nomeTurma,
-      Professor: professor,
+  const dados = [
+    {
+      "Nome do Aluno": nomeAluno,
+      ra: ra,
+      turma: turma,
       Turno: turno,
-    };
-
-  //exibindo o teste no navegador
+    },
+  ];
+  
   console.log(dados);
 
   //configurando a requisição POST
@@ -48,10 +48,10 @@ function enviarDados() {
   console.log(window.location)
 
   //Enviando a requisição POST para o servidor
-  fetch(window.location.origin + '/turma', option).then(teste => {
+  fetch(window.location.origin + '/aluno', option).then(teste => {
     teste.text().then(id =>{
     //Exibindo a resposta do servidor na página
-    document.getElementById("inserirTurma").innerHTML+=`<a target="__blank" href="/turmas/${id}">`  +id+'</a>'
+    document.getElementById("inserirAluno").innerHTML+=`<a target="__blank" href="/turmas/${id}">`  +id+'</a>'
       console.log(id + "este é o text")
   })
   }).catch(e => {
@@ -61,12 +61,12 @@ function enviarDados() {
   toggleModal();
 
   // Limpar os campos de entrada
-  nomeTurmaInput.value = "";
-  professorInput.value = "";
+  nomeAlunoInput.value = "";
+  raInput.value = "";
+  turmaInput.value = "";
   turnoInput.value = "";
 }
 
-//Adicionando evento para fechar o modal ao click no button ou apertar tecla enter
 document.querySelectorAll("input").forEach((input) => {
   input.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
